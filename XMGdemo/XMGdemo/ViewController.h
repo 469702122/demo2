c2681507be80212c8eed7d42950c63cb2e548aa5
周小帅 <zhouxiaoshuai@zhouxiaoshuaidemacbook-air.local>
2016-12-01 16:24:56 +0800

    初始化空的仓库,提交到本地

---
+++ XMGdemo/XMGdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMGdemo
+//
+//  Created by 周小帅 on 16/12/1.
+//  Copyright © 2016年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
